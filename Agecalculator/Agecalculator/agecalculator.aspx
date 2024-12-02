79046ef4b4216493611df75d078658b57b03e6d6
AliAkbar990 <akbarali9104515429@gmail.com>
2024-12-02 21:49:58 +0530

    Initial commit

---
+++ Agecalculator/Agecalculator/agecalculator.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="agecalculator.aspx.cs" Inherits="Agecalculator.agecalculator" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="select birthyear"></asp:Label>
+        <asp:TextBox ID="txt_brt_dte" runat="server" TextMode="Date"></asp:TextBox><br />
+
+        <asp:Label ID="Label2" runat="server" Text="select current date"></asp:Label>
+        <asp:TextBox ID="txt_crnt_date" runat="server" TextMode="Date"></asp:TextBox><br />
+
+        <asp:Button ID="Button1" runat="server" Text="Calculate Age" 
+            onclick="Button1_Click" />
+    </div>
+    
+    </form>
+</body>
+</html>
---
+++ Agecalculator/Agecalculator/agecalculator.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Agecalculator
+{
+    public partial class agecalculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            /* for birthdate */
+            string birthDate=txt_brt_dte.Text;
+            int birthYear = Convert.ToDateTime(birthDate).Year;
+            int birthMonth = Convert.ToDateTime(birthDate).Month;
+            int birthDay = Convert.ToDateTime(birthDate).Day;
+
+
+            /* for current_date */
+            string curntDate = txt_crnt_date.Text;
+            int crntYear = Convert.ToDateTime(curntDate).Year;
+            int crntMonth = Convert.ToDateTime(curntDate).Month;
+            int crnDay = Convert.ToDateTime(curntDate).Day;
+
+            //Response.Write("Birth Year "+birthYear);
+            //Response.Write("Current Year "+ crntYear);
+
+            /* for calculating age */
+            int ageYear = crntYear - birthYear;
+            int ageMonth;
+            int ageDay;
+
+            /* for calculating month difference */
+            int monthDifference = crntMonth - birthMonth;
+            /* for calculating days difference */
+            int daysDifference = crnDay - birthDay;
+
+            if (daysDifference <= 0)
+            {
+                ageDay = 0;
+            }
+            else {
+                ageDay = daysDifference; 
+            }
+
+            if (monthDifference <= 0)
+            {
+                ageMonth = 0;
+            }
+            else
+            {
+                ageMonth = monthDifference;
+            }
+
+
+
+            Response.Write("Your age is :" + ageDay + "/" + ageMonth + "/" + ageYear);
+        }
+    }
+}
